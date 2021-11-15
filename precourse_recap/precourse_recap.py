4ab46ea1c16228fdf5e508b06ddbc1de40ced370
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-15 22:31:53 +0000

    my first commit

---
+++ precourse_recap/precourse_recap.py
@@ -0,0 +1,10 @@
+#In the file, create a simple Python guessing game using:
+#-User Input
+#-If-Else statement
+
+guess = input("Guess my age: ")
+if guess == "26":
+    print("correct!")
+else:
+    print("WRONG")
+
